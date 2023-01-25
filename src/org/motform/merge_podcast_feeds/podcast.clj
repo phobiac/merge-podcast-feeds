fbca624fea85d75d2d94cf8cfdb15d1657a61e5a
motform <love.lagerkvist@protonmail.com>
2023-01-25 15:38:29 +0100

    Merge `date` and `xml` into `podcast`

--- src/org/motform/merge_podcast_feeds/podcast.clj
+++ src/org/motform/merge_podcast_feeds/podcast.clj
@@ -1,33 +1,123 @@
 (ns org.motform.merge-podcast-feeds.podcast
-  "Namespace for assembling the metadata entries in <channel>.
-  `config->channel` eats the metadata portion of the configuration file
-  which it silently assumes to be validated by `org.motform.merge-podcast-feeds.config`.
+  "Namespace for assembling the podcast feed.
+  `make-channel!` eats the metadata portion of the configuration file
+  and makes the <channel> as hiccup, ready to be used when assembling
+  the final, merged feed with `assemble-feed!`.
 
-  It deals with the hiccup formatted data, whereas `org.motform.merge-podcast-feeds.xml`
-  deals with `clojure.data.xml`s xml maps and zippers.
+  `web-feed` returns the indented string representation, while `output-test-feed`
+  can be used for debugging config files and feed links.
 
-  The process is specefic to the prescribed format of the config file.
-  I decided against listening to those nasty generalisation instincts.
-  As such, hard-coded if's abound."
-  (:require [clojure.data.xml :as xml]
-            [org.motform.merge-podcast-feeds.config :as config]
-            [org.motform.merge-podcast-feeds.xml    :as motform.xml]))
+  This namespace assumes all config validation is taken care of by `org.motform.merge-podcast-feeds.config`."
+  (:require [clojure.data.xml     :as xml]
+            [clojure.data.zip.xml :as zip-xml]
+            [clojure.java.io      :as io]
+            [clojure.zip          :as zip]
+            [org.motform.merge-podcast-feeds.config :as config])
+  (:import (java.time ZonedDateTime)
+           (java.time.format DateTimeFormatter)))
 
 (def *state
-  (atom #:state{:feed    nil
-                :channel nil}))
+  (atom #:state{:channel  nil
+                :xml-feed nil
+                :str-feed nil}))
 
-(defn feed []
-  (get @*state :state/feed))
+
+;;; Utility
 
 (defn- concatv
   "Return a vector representing the concatenation of the elements in the supplied colls."
   [x y]
   (into [] (concat x y)))
 
+
+;; Date handling
+
+(defn- parse-RFC1123-date
+  "Return comparable `ZonedDateTime` instance from parsed RFC1123 date string."
+  [^String rfc1123-date]
+  (let [formatter (DateTimeFormatter/RFC_1123_DATE_TIME)]
+    (ZonedDateTime/parse rfc1123-date formatter)))
+
+(defn- RFC1123-date
+  "Return string current time formatted per RFC1123."
+  []
+  (let [formatter (DateTimeFormatter/RFC_1123_DATE_TIME)
+        now       (ZonedDateTime/now)]
+    (. formatter format now)))
+
+
+;;; XML wrangling
+
+(defn- parse-xml-feed-and-return-items
+  "Parse xml at `feed-url` and return seq with podcast items, i.e. all <item>."
+  [feed-url]
+  (with-open [stream (io/input-stream feed-url)]
+    (let [xml (xml/parse stream)
+          items (zip-xml/xml-> (zip/xml-zip xml) :channel :item)]
+      (map zip/node items))))
+
+(defn- zip-from-root-to-episode-insertion
+  "Goes from the root of `xml-zip`, assumed to be <channel>
+  to the loc where that is, episodes are expeceted."
+  [xml-zip]
+  (-> xml-zip zip/down zip/rightmost))
+
+(defn- insert-rightmost
+  "Insert `items` at the loc of `xml-zip` in list order."
+  [xml-zip items]
+  (reduce
+   (fn [xml-zip item] (zip/insert-right xml-zip item))
+   (-> xml-zip zip/down zip/rightmost)
+   items))
+
+(defn- publication-date
+  "Return the podcast item `:pubDate`, a string in RFC-1123 format."
+  [item]
+  (-> (zip/xml-zip item)
+      (zip-xml/xml1-> :pubDate)
+      zip/node
+      :content
+      first))
+
+(defn- collect-and-sort-feeds
+  "Return seq of <item> nodes from feed urls in `feeds`."
+  [feeds]
+  (->> feeds
+       (map parse-xml-feed-and-return-items)
+       (apply concat)
+       (sort-by (comp parse-RFC1123-date publication-date))))
+
+(defn- append-podcast-feeds
+  "Append `feeds` to `xml` in order at rightmost position."
+  [xml feeds]
+  (-> xml
+      zip/xml-zip
+      zip-from-root-to-episode-insertion
+      (insert-rightmost feeds)
+      zip/root))
+
+(defn hiccup-channel->xml-with-pubDate
+  "Return map XML of feed, contaning a hiccup formatted <channel>
+  in an <rss> tag containing the most common podcasting xml namespaces.
+
+  TODO:
+  It might be a better idea to include the map of namespaces as an argument,
+  but the current state of the program (230125) does not support those anyway."
+  [channel]
+  (xml/sexp-as-element
+   [:rss
+    {:version "2.0"
+     :xmlns/itunes  "http://www.itunes.com/dtds/podcast-1.0.dtd"
+     :xmlns/atom    "http://www.w3.org/2005/Atom"
+     :xmlns/podcast "https://github.com/Podcastindex-org/podcast-namespace/blob/main/docs/1.0.md"
+     :xmlns/content "http://purl.org/rss/1.0/modules/content/"}
+    (conj channel [:pubDate (RFC1123-date)])]))
+
+
+;;; <rss> and <channel> assembly
+
 ;; TODO: Register these based on user config?
-;; They are curently mirroring the namespaces used in
-;;`org.motform.merge-podcast-feeds.xml/hiccup-channel->xml-with-pubDate`
+;;       They are curently mirroring the namespaces used in `hiccup-channel->xml-with-pubDate`.
 (xml/alias-uri 'atom    "http://www.w3.org/2005/Atom")
 (xml/alias-uri 'content "http://purl.org/rss/1.0/modules/content/")
 (xml/alias-uri 'itunes  "http://www.itunes.com/dtds/podcast-1.0.dtd")
@@ -94,7 +184,7 @@
     :rel  "self"
     :type "application/rss+xml"]])
 
-(defn ->hiccup-channel
+(defn- ->hiccup-channel
   "Return hiccup representation of `<rss><channel> ... </channel></rss>`.
   To be consumed by `clojure.data.xml/sexp-as-element`.
   It will most likely be done by `org.motform.merge-podcast-feeds.xml/hiccup-feed->xml-with-pubDate`.
@@ -109,20 +199,29 @@
       (concatv (itunes-tags (get  metadata :metadata/itunes)))
       (conj [:generator "https://github.com/motform/merge-podcast-feeds"])))
 
-;; TODO: Take into account the feed updating, depending on how we do it
-(defn assemble-feed! []
-  (let [feed-urls   (config/get-in [:config/feeds])
-        channel     (get @*state :state/channel)
-        feeds       (motform.xml/collect-and-sort-feeds feed-urls)
-        xml-no-feed (motform.xml/hiccup-channel->xml-with-pubDate channel)
-        xml         (motform.xml/append-podcast-feeds xml-no-feed feeds)]
-    (swap! *state assoc :state/feed xml)))
+
+;;; Main functions
 
 (defn make-channel! []
   (let [metadata (config/get-in [:config/metadata])
         channel  (->hiccup-channel metadata)]
     (swap! *state assoc :state/channel channel)))
 
+;; TODO: Define a process for feed updates
+(defn assemble-feed! []
+  (let [feed-urls   (config/get-in [:config/feeds])
+        channel     (get @*state :state/channel)
+        feeds       (collect-and-sort-feeds feed-urls)
+        xml-no-feed (hiccup-channel->xml-with-pubDate channel)
+        xml         (append-podcast-feeds xml-no-feed feeds)]
+    (swap! *state assoc
+           :state/xml-feed xml
+           :state/str-feed (xml/indent-str xml))))
+
 (defn output-test-feed []
   (let [path (config/get-in [:config/xml-file-path])]
-    (motform.xml/emit-indent-xml (feed) path)))
+    (with-open [output-file (io/writer path)]
+      (xml/indent (get @*state :state/xml-feed) output-file))))
+
+(defn web-feed []
+  (get @*state :state/str-feed))
